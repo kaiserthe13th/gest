42236233ccf42390b2ae23c61a767d04ffdb374c
Kerem Göksu <superkerem13@gmail.com>
2026-05-19 17:57:11 +0200

    feat: create a basic string api

---
+++ include/gest/str.h
@@ -0,0 +1,78 @@
+#ifndef GEST_STR_H
+#define GEST_STR_H
+
+#include <assert.h>
+#include <gest/allocator.h>
+#include <gest/common.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <stddef.h>
+
+typedef struct GestStr GestStr;
+typedef struct GestStrBuf GestStrBuf;
+
+struct GestStr {
+    size_t length;
+    const uint8_t *chars;
+};
+
+#define gstrlit(s) ((GestStr){sizeof(s) - 1, s})
+
+struct GestStrBuf {
+    GestAllocator *alloc;
+    size_t capacity;
+    size_t length;
+    uint8_t *chars;
+};
+
+typedef struct GestStr GestBaseStrIter;
+
+inline static GestBaseStrIter gestStrToBaseIter(GestStr s) {
+    return s;
+}
+
+inline static GestStrBuf gestEmptyStrBuf(GestAllocator *alloc) {
+    return (GestStrBuf){.alloc = alloc};
+}
+
+inline static GestStrBuf gestStrBufFromStr(GestAllocator *alloc, GestStr s) {
+    char *chars = gestAllocMany(&alloc, char, s.length, 0);
+    memcpy(chars, s.chars, s.length);
+    return (GestStrBuf){ .alloc = alloc, .capacity = s.length, .chars = chars, .length = s.length };
+}
+
+inline static GestStr gestStrFromStrBuf(GestStrBuf buf) {
+    return (GestStr){.length = buf.length, .chars = buf.chars ? buf.chars : ""};
+}
+
+/// Finds the index at which `needle` is first encountered within `haystack`.
+/// @param haystack The string being searched inside of.
+/// @param needle The substring that is being searched.
+/// @return the index at which in the haystack that the needle was found starting at if found, `-1` if not found
+GEST_API ssize_t gestStrIndexSubstr(GestStr haystack, GestStr needle);
+/// Checks whether `haystack` starts with `needle`.
+/// @param haystack The string being searched inside of.
+/// @param needle The substring that is being searched.
+/// @return `1` if true, `0` if false
+GEST_API int gestStrBeginsWith(GestStr haystack, GestStr needle);
+/// Checks whether `haystack` ends with `needle`.
+/// @param haystack The string being searched inside of.
+/// @param needle The substring that is being searched.
+/// @return `1` if true, `0` if false
+GEST_API int gestStrEndsWith(GestStr haystack, GestStr needle);
+/// If `haystack` starts with `needle`, returns a substring of haystack which skips it.
+/// @param haystack The string being searched inside of.
+/// @param needle The substring that is being searched.
+/// @return The substring, or the haystack itself if nothing was stripped
+GEST_API GestStr gestStrStripPrefix(GestStr haystack, GestStr needle);
+/// If `haystack` ends with `needle`, returns a substring of haystack which stops just before it.
+/// @param haystack The string being searched inside of.
+/// @param needle The substring that is being searched.
+/// @return The substring, or the haystack itself if nothing was stripped
+GEST_API GestStr gestStrStripPostfix(GestStr haystack, GestStr needle);
+
+GEST_API int gestStrBufPushU32Char(GestStrBuf *buf, GestU32Char codepoint);
+GEST_API int gestStrBufPushStr(GestStrBuf *buf, const GestStr s);
+
+#endif // GEST_STR_H
---
+++ src/gest/str.c
@@ -0,0 +1,99 @@
+#include <gest/error.h>
+#include <gest/str.h>
+#include <gest/unicode.h>
+
+ssize_t gestStrIndexSubstr(GestStr haystack, GestStr needle) {
+    if (needle.length == 0) return 0;
+    if (needle.length > haystack.length) return -1;
+
+#ifdef _DEBUG
+    assert(haystack.chars && needle.chars);
+#endif // _DEBUG
+
+    size_t diff = haystack.length - needle.length;
+    for (size_t i = 0; i <= diff; i++) {
+        if (haystack.chars[i] == needle.chars[0]
+            && memcmp(&haystack.chars[i], needle.chars, needle.length) == 0) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+int gestStrBeginsWith(GestStr haystack, GestStr needle) {
+    if (needle.length == 0) return 1;
+    if (needle.length > haystack.length) return 0;
+
+#ifdef _DEBUG
+    assert(haystack.chars && needle.chars);
+#endif // _DEBUG
+
+    return !memcmp(haystack.chars, needle.chars, needle.length);
+}
+
+int gestStrEndsWith(GestStr haystack, GestStr needle) {
+    if (needle.length == 0) return 1;
+    if (needle.length > haystack.length) return 0;
+
+#ifdef _DEBUG
+    assert(haystack.chars && needle.chars);
+#endif // _DEBUG
+
+    return !memcmp(&haystack.chars[haystack.length - needle.length], needle.chars, needle.length);
+}
+
+GestStr gestStrStripPrefix(GestStr haystack, GestStr needle) {
+    if (gestStrBeginsWith(haystack, needle)) {
+        return (GestStr){ .length = haystack.length - needle.length,
+                          .chars = &haystack.chars[needle.length] };
+    }
+    return haystack;
+}
+
+GestStr gestStrStripPostfix(GestStr haystack, GestStr needle) {
+    if (gestStrEndsWith(haystack, needle)) {
+        return (GestStr){ .length = haystack.length - needle.length, .chars = haystack.chars };
+    }
+    return haystack;
+}
+
+int gestStrBufPushU32Char(GestStrBuf *buf, GestU32Char codepoint) {
+    struct GestUTF8EncodedChar__ utf8EncCh = gestUniC32EncodeUTF8(codepoint);
+    const GestStr utf8Str = { .chars = utf8EncCh.enc, .length = utf8EncCh.length };
+    return gestStrBufPushStr(buf, utf8Str);
+}
+
+inline int gestStrBufGrowIfNeeded__(GestStrBuf *buf, size_t requiredCapacity) {
+    gestErrno = GEST_ENONE;
+    size_t newCapacity = buf->capacity;
+    while (requiredCapacity > newCapacity) {
+        if (newCapacity >= SIZE_MAX / 2) return gestErrno = GEST_EARITH;
+        newCapacity = newCapacity * 2 + 1;
+    }
+    if (newCapacity != buf->capacity) {
+        uint8_t *newChars = gestAlloc(
+            buf->alloc,
+            buf->chars,
+            buf->capacity * sizeof(uint8_t),
+            newCapacity * sizeof(uint8_t),
+            alignof(uint8_t),
+            0
+        );
+        if (!newChars) return gestErrno;
+        buf->chars = newChars;
+        buf->capacity = newCapacity;
+    }
+    return GEST_ENONE;
+}
+
+int gestStrBufPushStr(GestStrBuf *buf, const GestStr s) {
+    gestErrno = GEST_ENONE;
+    // No-op for 0-length strings
+    if (s.length == 0) return GEST_ENONE;
+    if (s.length > SIZE_MAX - buf->length) return gestErrno = GEST_EARITH;
+    if (gestStrBufGrowIfNeeded__(buf, buf->length + s.length)) return gestErrno;
+
+    memcpy(&buf->chars[buf->length], s.chars, s.length);
+    buf->length += s.length;
+    return GEST_ENONE;
+}
